7aaa1e6ba6dcef26058f0fe9ccf4cebc1e77153a
Zehua-Chen <peterchen06242000@outlook.com>
2020-12-16 09:29:57 -0600

    [refactor] Put MakeTarget into Separate File

--- src/LaTeXTools.Build/Generators/Makefile.cs
+++ src/LaTeXTools.Build/Generators/Makefile.cs
@@ -4,56 +4,6 @@ using System.Threading.Tasks;
 
 namespace LaTeXTools.Build.Generators
 {
-    public class MakeTarget
-    {
-        public string Name { get; set; } = "x";
-        public bool IsPhony { get; set; } = false;
-        public List<string> Commands { get; private set; } = new List<string>();
-        public List<string> Dependencies { get; private set; } = new List<string>();
-        public List<string> OrderOnlyDependencies { get; private set; } = new List<string>();
-
-        public async ValueTask Write(StreamWriter writer)
-        {
-            bool finishWithEndLine = false;
-
-            if (this.IsPhony)
-            {
-                await writer.WriteLineAsync($".PHONY: {this.Name}");
-            }
-
-            await writer.WriteAsync($"{this.Name}: ");
-
-            if (this.Dependencies.Count > 0)
-            {
-                string dependencies = string.Join(" ", this.Dependencies);
-                await writer.WriteAsync(dependencies);
-            }
-
-            if (this.OrderOnlyDependencies.Count > 0)
-            {
-                string orderOnlyDependencies = string.Join(" ", this.OrderOnlyDependencies);
-                await writer.WriteAsync($" | {orderOnlyDependencies}");
-            }
-
-            if (this.Commands.Count > 0)
-            {
-                await writer.WriteLineAsync();
-
-                foreach (var command in this.Commands)
-                {
-                    await writer.WriteLineAsync($"\t{command}");
-                }
-
-                finishWithEndLine = true;
-            }
-
-            if (!finishWithEndLine)
-            {
-                await writer.WriteLineAsync();
-            }
-        }
-    }
-
     public class Makefile
     {
         public List<MakeTarget> Targets { get; private set; } = new List<MakeTarget>();
