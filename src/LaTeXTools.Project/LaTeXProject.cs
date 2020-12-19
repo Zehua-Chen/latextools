0288f90221cfe17e08b8566b5065eafacb28a056
Zehua-Chen <peterchen06242000@outlook.com>
2020-12-19 15:44:48 -0600

    [docs] Add Comments to LaTeXTools.Project

--- src/LaTeXTools.Project/LaTeXProject.cs
+++ src/LaTeXTools.Project/LaTeXProject.cs
@@ -6,26 +6,53 @@ using System.IO;
 
 namespace LaTeXTools.Project
 {
-    public class LaTeXProject
+    /// <summary>
+    /// Models a LaTeX project
+    /// </summary>
+    public sealed class LaTeXProject
     {
+        /// <summary>
+        /// The latex engine that we use.
+        /// </summary>
         [JsonPropertyName("latex")]
         public string LaTeX { get; set; } = "pdflatex";
 
+        /// <summary>
+        /// Where to place outputs
+        /// </summary>
         [JsonPropertyName("bin")]
         public string Bin { get; set; } = "bin";
 
+        /// <summary>
+        /// Entry <c>.tex</c> file of the project
+        /// </summary>
         [JsonPropertyName("entry")]
         public string Entry { get; set; } = "index.tex";
 
+        /// <summary>
+        /// Bibliography file of the project
+        /// </summary>
         [JsonPropertyName("bib")]
         public string Bib { get; set; } = "none";
 
+        /// <summary>
+        /// Files/directories included by <c>Entry</c>
+        /// </summary>
         [JsonPropertyName("includes")]
         public string[] Includes { get; set; } = new string[] { };
 
+        /// <summary>
+        /// Working directory of the project, i.e. the folder in which the project file is in
+        /// </summary>
+        /// <value></value>
         [JsonIgnore]
         public string WorkingDirectory { get; set; } = Environment.CurrentDirectory;
 
+        /// <summary>
+        /// Write the project to a file at path
+        /// </summary>
+        /// <param name="path">the path of the file to be written to</param>
+        /// <returns></returns>
         public async ValueTask WriteAsync(string path)
         {
             using FileStream file = File.OpenWrite(path);
@@ -37,6 +64,12 @@ namespace LaTeXTools.Project
             await JsonSerializer.SerializeAsync(file, this, options);
         }
 
+        /// <summary>
+        /// Search up from the current working directory to find a project file with a specified
+        /// name
+        /// </summary>
+        /// <param name="filename">the name of the project file to find</param>
+        /// <returns><c>null</c> if not found, otherwise return the project</returns>
         public static async ValueTask<LaTeXProject?> FindAsync(string filename)
         {
             string current = Environment.CurrentDirectory;
@@ -64,6 +97,11 @@ namespace LaTeXTools.Project
             return null;
         }
 
+        /// <summary>
+        /// Load a project from a project file.
+        /// </summary>
+        /// <param name="path">the path of the file to load from</param>
+        /// <returns>The loaded project</returns>
         public static async ValueTask<LaTeXProject> LoadAsync(string path)
         {
             if (!Path.IsPathRooted(path))
--- src/LaTeXTools.Project/LaTexProject+Paths.cs
+++ src/LaTeXTools.Project/LaTexProject+Paths.cs
@@ -5,24 +5,45 @@ namespace LaTeXTools.Project
 {
     public static class LaTeXProjectPathsExtensions
     {
+        /// <summary>
+        /// Get the path to <c>.aux</c> file of this project
+        /// </summary>
+        /// <param name="project">the project in question</param>
+        /// <returns>path to the <c>.aux</c> file</returns>
         public static string GetAUXPath(this LaTeXProject project)
         {
             string document = Path.GetFileNameWithoutExtension(project.Entry);
             return Path.Combine(project.Bin, string.Format("{0}.aux", document));
         }
 
+        /// <summary>
+        /// Get the path to <c>.pdf</c> file of this project
+        /// </summary>
+        /// <param name="project">the project in question</param>
+        /// <returns>path to the <c>.pdf</c> file</returns>
         public static string GetPDFPath(this LaTeXProject project)
         {
             string document = Path.GetFileNameWithoutExtension(project.Entry);
             return Path.Combine(project.Bin, string.Format("{0}.pdf", document));
         }
 
+        /// <summary>
+        /// Get the path to <c>.log</c> file of this project
+        /// </summary>
+        /// <param name="project">the project in question</param>
+        /// <returns>path to the <c>.log</c> file</returns>
         public static string GetLogPath(this LaTeXProject project)
         {
             string document = Path.GetFileNameWithoutExtension(project.Entry);
             return Path.Combine(project.Bin, string.Format("{0}.log", document));
         }
 
+        /// <summary>
+        /// Get a list of the files that are dependencies to building the project. This would
+        /// include <c>project.Entry</c>
+        /// </summary>
+        /// <param name="project">the project in question</param>
+        /// <returns>a list of file paths</returns>
         public static List<string> GetDependencyPaths(this LaTeXProject project)
         {
             var dependencies = new List<string>();
