3903b4cee867be8bec4e406e899780a9507936ee
Zehua-Chen <peterchen06242000@outlook.com>
2021-03-27 21:07:40 -0500

    [test] Test File Content Comparison Tests

--- src/LaTeXTools.Build/Tasks/FileContentComparisonsTask.cs
+++ src/LaTeXTools.Build/Tasks/FileContentComparisonsTask.cs
@@ -7,8 +7,33 @@ namespace LaTeXTools.Build.Tasks
 {
     public class FileContentComparisonsTask : BuildTask
     {
-        public BuildTask? Task { get; set; }
-        public IEnumerable<FileContentComparison?>? FileContentComparisons { get; set; }
+        /// <summary>
+        /// The task ot execute if there is a file content comparison returns false.
+        /// </summary>
+        /// <remarks>
+        /// An instance of <c>BuildTask</c> by default
+        /// </remarks>
+        /// <value></value>
+        public BuildTask Task { get; set; }
+
+        /// <summary>
+        /// A list of file comparisons
+        /// </summary>
+        ///
+        /// <remarks>
+        /// <list>
+        ///     <item>If an item is null then it is not executed</item>
+        ///     <item>By default an empty array</item>
+        /// </list>
+        /// </remarks>
+        /// <value></value>
+        public IEnumerable<FileContentComparison?> FileContentComparisons { get; set; }
+
+        public FileContentComparisonsTask() : base()
+        {
+            Task = new BuildTask();
+            FileContentComparisons = new FileContentComparison[0];
+        }
 
         /// <summary>
         /// Execute <c>Task</c> if any file content comparisons returns false
@@ -35,14 +60,25 @@ namespace LaTeXTools.Build.Tasks
                 // there is a content mismatch
                 if (!comparison)
                 {
-                    if (Task != null)
-                    {
-                        await Task.RunAsync(logger);
-                    }
+                    await Task.RunAsync(logger);
 
                     break;
                 }
             }
+        }
+
+        public override bool Equals(BuildTask? other)
+        {
+            if (other == null || !(other is FileContentComparisonsTask))
+            {
+                return false;
+            }
+
+            var otherComparisonTask = (FileContentComparisonsTask)other;
+
+            return base.Equals(other)
+                && this.Task.Equals(otherComparisonTask.Task)
+                && FileContentComparisons.SequenceEqual(otherComparisonTask.FileContentComparisons);
         }
     }
 }
