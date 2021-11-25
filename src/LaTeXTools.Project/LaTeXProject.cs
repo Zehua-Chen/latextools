c370af907c368a52f0721bbd749e4e4052de3ef0
Zehua-Chen <zehua-chen@outlook.com>
2021-11-25 15:44:18 -0500

    [refactor] Adopt Top Level Namespace

--- src/LaTeXTools.Project/LaTeXProject.cs
+++ src/LaTeXTools.Project/LaTeXProject.cs
@@ -5,130 +5,140 @@ using System.Text.Json.Serialization;
 using System.IO;
 using System.IO.Abstractions;
 
-namespace LaTeXTools.Project
+namespace LaTeXTools.Project;
+
+// [JsonSerializable(typeof(LaTeXProject))]
+// internal partial class LaTeXProjectContext : JsonSerializerContext
+// {
+
+// }
+
+/// <summary>
+/// Models a LaTeX project
+/// </summary>
+public sealed class LaTeXProject
 {
     /// <summary>
-    /// Models a LaTeX project
+    /// The latex engine that we use.
     /// </summary>
-    public sealed class LaTeXProject
+    [JsonPropertyName("latex")]
+    public string LaTeX { get; set; } = "pdflatex";
+
+    /// <summary>
+    /// Where to place outputs
+    /// </summary>
+    [JsonPropertyName("bin")]
+    public string Bin { get; set; } = "bin";
+
+    /// <summary>
+    /// Entry <c>.tex</c> file of the project
+    /// </summary>
+    [JsonPropertyName("entry")]
+    public string Entry { get; set; } = "index.tex";
+
+    /// <summary>
+    /// Bibliography file of the project
+    /// </summary>
+    [JsonPropertyName("bib")]
+    public string Bib { get; set; } = "none";
+
+    /// <summary>
+    /// If to enable glossary for this project
+    /// </summary>
+    [JsonPropertyName("glossary")]
+    public bool Glossary { get; set; } = false;
+
+    /// <summary>
+    /// Files/directories included by <c>Entry</c>
+    /// </summary>
+    [JsonPropertyName("includes")]
+    public string[] Includes { get; set; } = new string[] { };
+
+    /// <summary>
+    /// Working directory of the project, i.e. the folder in which the project file is in
+    /// </summary>
+    /// <value></value>
+    [JsonIgnore]
+    public string WorkingDirectory { get; set; } = Environment.CurrentDirectory;
+
+    /// <summary>
+    /// Write the project to a file at path
+    /// </summary>
+    /// <param name="path">the path of the file to be written to</param>
+    /// <returns></returns>
+    public async ValueTask WriteAsync(string path, IFile file)
     {
-        /// <summary>
-        /// The latex engine that we use.
-        /// </summary>
-        [JsonPropertyName("latex")]
-        public string LaTeX { get; set; } = "pdflatex";
-
-        /// <summary>
-        /// Where to place outputs
-        /// </summary>
-        [JsonPropertyName("bin")]
-        public string Bin { get; set; } = "bin";
-
-        /// <summary>
-        /// Entry <c>.tex</c> file of the project
-        /// </summary>
-        [JsonPropertyName("entry")]
-        public string Entry { get; set; } = "index.tex";
-
-        /// <summary>
-        /// Bibliography file of the project
-        /// </summary>
-        [JsonPropertyName("bib")]
-        public string Bib { get; set; } = "none";
-
-        /// <summary>
-        /// If to enable glossary for this project
-        /// </summary>
-        [JsonPropertyName("glossary")]
-        public bool Glossary { get; set; } = false;
-
-        /// <summary>
-        /// Files/directories included by <c>Entry</c>
-        /// </summary>
-        [JsonPropertyName("includes")]
-        public string[] Includes { get; set; } = new string[] { };
-
-        /// <summary>
-        /// Working directory of the project, i.e. the folder in which the project file is in
-        /// </summary>
-        /// <value></value>
-        [JsonIgnore]
-        public string WorkingDirectory { get; set; } = Environment.CurrentDirectory;
-
-        /// <summary>
-        /// Write the project to a file at path
-        /// </summary>
-        /// <param name="path">the path of the file to be written to</param>
-        /// <returns></returns>
-        public async ValueTask WriteAsync(string path, IFile file)
+        using Stream fileStream = file.OpenWrite(path);
+        JsonSerializerOptions options = new JsonSerializerOptions()
         {
-            using Stream fileStream = file.OpenWrite(path);
-            JsonSerializerOptions options = new JsonSerializerOptions()
-            {
-                WriteIndented = true,
-            };
+            WriteIndented = true,
+        };
 
-            await JsonSerializer.SerializeAsync(fileStream, this, options);
+        // options.AddContext<LaTeXProjectContext>();
+
+        await JsonSerializer.SerializeAsync(fileStream, this, options);
+    }
+
+    /// <summary>
+    /// Search up from the current working directory to find a project file with a specified
+    /// name
+    /// </summary>
+    /// <param name="filename">the name of the project file to find</param>
+    /// <returns><c>null</c> if not found, otherwise return the project</returns>
+    public static async ValueTask<LaTeXProject?> FindAsync(string filename)
+    {
+        string current = Environment.CurrentDirectory;
+        string root = Path.GetPathRoot(current)!;
+
+        while (!File.Exists(Path.Combine(current, filename)) || current == root)
+        {
+            string? parent = Path.GetDirectoryName(current);
+
+            if (parent == null)
+            {
+                return null;
+            }
+
+            current = parent;
         }
 
-        /// <summary>
-        /// Search up from the current working directory to find a project file with a specified
-        /// name
-        /// </summary>
-        /// <param name="filename">the name of the project file to find</param>
-        /// <returns><c>null</c> if not found, otherwise return the project</returns>
-        public static async ValueTask<LaTeXProject?> FindAsync(string filename)
+        string path = Path.Combine(current, filename);
+
+        if (File.Exists(path))
         {
-            string current = Environment.CurrentDirectory;
-            string root = Path.GetPathRoot(current)!;
-
-            while (!File.Exists(Path.Combine(current, filename)) || current == root)
-            {
-                string? parent = Path.GetDirectoryName(current);
-
-                if (parent == null)
-                {
-                    return null;
-                }
-
-                current = parent;
-            }
-
-            string path = Path.Combine(current, filename);
-
-            if (File.Exists(path))
-            {
-                return await LoadAsync(path);
-            }
-
-            return null;
+            return await LoadAsync(path);
         }
 
-        /// <summary>
-        /// Load a project from a project file.
-        /// </summary>
-        /// <param name="path">the path of the file to load from</param>
-        /// <returns>The loaded project</returns>
-        public static async ValueTask<LaTeXProject> LoadAsync(string path)
+        return null;
+    }
+
+    /// <summary>
+    /// Load a project from a project file.
+    /// </summary>
+    /// <param name="path">the path of the file to load from</param>
+    /// <returns>The loaded project</returns>
+    public static async ValueTask<LaTeXProject> LoadAsync(string path)
+    {
+        if (!Path.IsPathRooted(path))
         {
-            if (!Path.IsPathRooted(path))
-            {
-                throw new ArgumentException($"{path} is not rooted!");
-            }
-
-            using var stream = File.Open(path, FileMode.Open);
-            string directory = Path.GetDirectoryName(path)!;
-
-            var project = await JsonSerializer.DeserializeAsync<LaTeXProject>(stream);
-
-            if (project == null)
-            {
-                throw new Exception($"Failed to parse project at {path}");
-            }
-
-            project.WorkingDirectory = directory;
-
-            return project;
+            throw new ArgumentException($"{path} is not rooted!");
         }
+
+        using var stream = File.Open(path, FileMode.Open);
+        string directory = Path.GetDirectoryName(path)!;
+
+        var options = new JsonSerializerOptions();
+        // options.AddContext<LaTeXProjectContext>();
+
+        var project = await JsonSerializer.DeserializeAsync<LaTeXProject>(stream, options);
+
+        if (project == null)
+        {
+            throw new Exception($"Failed to parse project at {path}");
+        }
+
+        project.WorkingDirectory = directory;
+
+        return project;
     }
 }
