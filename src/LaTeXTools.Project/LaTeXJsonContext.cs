21f6e0ea783637b25d41ff9ca4d14c6b2a340c6b
Zehua-Chen <zehua-chen@outlook.com>
2022-04-17 16:42:12 -0400

    [feat] Use Json Serializer

---
+++ src/LaTeXTools.Project/LaTeXJsonContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LaTeXTools.Project;
+
+/// <summary>
+/// Json Context for serialization
+/// </summary>
+[JsonSerializable(typeof(LaTeXProject))]
+internal partial class LaTeXJsonContext : JsonSerializerContext {}
